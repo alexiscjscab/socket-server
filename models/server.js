b16c55f79062eb6923cf1a17aabe92c0d415be90
alexiscjscab <alexiscjscab@gmail.com>
2022-11-30 22:38:30 -0300

    agregar cors

--- models/server.js
+++ models/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const socketio = require('socket.io');
 const path = require('path');
 const Sockets = require('./sockets');
-
+const cors = require('cors');
 
 class Server {
   constructor() {
@@ -12,12 +12,13 @@ class Server {
 
     // Http Server
     this.server = http.createServer(this.app);
-    
-    // Configuraciones de sockets
-    this.io = socketio(this.server, {/*configuraciones*/});
 
+    // Configuraciones de sockets
+    this.io = socketio(this.server, {
+      /*configuraciones*/
+    });
   }
-  execute () {
+  execute() {
     // inicializar middlewares
     this.middlewares();
 
@@ -26,19 +27,32 @@ class Server {
     this.server.listen(this.port, () => {
       console.log(`Server corriendo en el piuerto ${this.port}`);
     });
+  }
 
-  } 
-
-  middlewares () {
+  middlewares() {
     // desplegar el directorio publico
-    this.app.use(express.static( path.resolve( __dirname, '../public')))
+    this.app.use(express.static(path.resolve(__dirname, '../public')));
+
+    this.app.use(cors());
+
+    this.app.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+      res.header('Access-Control-Allow-Credentials', 'true');
+      res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+      );
+      res.header(
+        'Access-Control-Allow-Methods',
+        'GET, POST, OPTIONS, PUT, DELETE'
+      );
+      next();
+    });
   }
 
-  configureSocket () {
-    new Sockets(this.io)
+  configureSocket() {
+    new Sockets(this.io);
   }
-
-};
-
+}
 
 module.exports = Server;
